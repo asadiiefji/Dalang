85bf5c0a91cbe09b9b9068e00df472e4cc61d012
asadiiefji <asadiiefji@gmail.com>
2023-05-16 12:30:06 +0700

    Initial Commit

---
+++ Dalang/DalangApp.swift
@@ -0,0 +1,17 @@
+//
+//  DalangApp.swift
+//  Dalang
+//
+//  Created by Mukhammad Miftakhul As'Adi on 16/05/23.
+//
+
+import SwiftUI
+
+@main
+struct DalangApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
